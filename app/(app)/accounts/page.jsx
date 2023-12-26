46750438e4d41a5ecaf2826963573c8f3e41a3a3
ahmadnurfadilah <ahmadnurfadilah22@gmail.com>
2023-12-26 10:25:24 +0700

    account: create new

--- app/(app)/accounts/page.jsx
+++ app/(app)/accounts/page.jsx
@@ -7,7 +7,11 @@ import { Label } from "@/components/ui/label";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { ArrowRightCircle, Plus } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthWeb5 } from "@/lib/auth";
+import { useLoading } from "@/lib/store";
+import protocolDefinition from "../../../public/protocol.json";
+import toast from "react-hot-toast";
 
 const dummy = [
   { id: 1, name: "Cash", amount: 120, category: "Cash" },
@@ -16,7 +20,64 @@ const dummy = [
 ];
 
 export default function Page() {
+  const { did, web5 } = useAuthWeb5();
+  const [accounts, setAccounts] = useState([]);
   const [open, setOpen] = useState(false);
+  const setLoading = useLoading((state) => state.setMsg);
+
+  useEffect(() => {
+    if (!did || !web5) return;
+    getRecord();
+  }, [did, web5]);
+
+  const getRecord = async () => {
+    const { records } = await web5.dwn.records.query({
+      message: {
+        filter: {
+          schema: protocolDefinition.types.account.schema,
+        },
+        dateSort: "createdAscending",
+      },
+    });
+
+    const newAccounts = [];
+    for (let record of records) {
+      const data = await record.data.json();
+      newAccounts.push({ id: record.id, ...data });
+    }
+    setAccounts(newAccounts);
+  };
+
+  const handleSubmit = async (values) => {
+    setLoading("Loading...");
+
+    const newAccount = {
+      "@type": "account",
+      name: values.name,
+      balance: parseInt(values.balance),
+      author: did,
+    };
+
+    try {
+      const { record } = await web5.dwn.records.create({
+        data: newAccount,
+        message: {
+          protocol: protocolDefinition.protocol,
+          protocolPath: "account",
+          schema: protocolDefinition.types.account.schema,
+          dataFormat: protocolDefinition.types.account.dataFormats[0],
+        },
+      });
+      getRecord();
+      toast.success("Success!");
+      setOpen(false);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to create a new account");
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -34,7 +95,7 @@ export default function Page() {
 
       <div className="container px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {dummy.map((i) => (
+          {accounts.map((i) => (
             <Link
               key={i.id}
               href="/"
@@ -42,7 +103,7 @@ export default function Page() {
             >
               <div className="flex-1 p-4">
                 <h2 className="mb-1 text-gray-500">{i.name}</h2>
-                <p className="font-mono font-bold text-xl">${i.amount}</p>
+                <p className="font-mono font-bold text-xl">${i.balance}</p>
               </div>
               <div className="shrink-0 bg-primary h-full rounded-r-2xl w-1/5 p-2 flex justify-center items-center text-dark/50 group-hover:w-1/4 transition-all group-hover:text-dark duration-500">
                 <ArrowRightCircle className="w-6 h-6" />
@@ -59,12 +120,7 @@ export default function Page() {
             name: Yup.string().required("Required"),
             balance: Yup.number().required("Required"),
           })}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
+          onSubmit={(values) => handleSubmit(values)}
         >
           {({ isSubmitting }) => (
             <Form className="space-y-2">
